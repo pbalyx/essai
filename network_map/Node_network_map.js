62d30b4dabf39fff9db829914e01f38f99449146
pbalyx <40723752+pbalyx@users.noreply.github.com>
2023-09-01 18:41:21 +0200

    ref tracés osm si planIGN

--- network_map/Node_network_map.js
+++ network_map/Node_network_map.js
@@ -29,13 +29,13 @@ var OSMLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.
 // region PlanIGN
 	var PlanIGNLayer = L.tileLayer('https://wxs.ign.fr/pratique/wmts/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fpng', {
 		maxZoom: 19,
-		attribution: '&copy; <a href="https://geoservices.ign.fr/planign">Plan IGN</a> | <a href="https://www.openstreetmap.org/user/pb07/diary">pb07</a>'
+		attribution: ' Carte: <a href="https://geoservices.ign.fr/planign">Plan IGN</a> | Tracés: <a href="https://www.openstreetmap.org">OpenStreetMap</a> | <a href="https://www.openstreetmap.org/user/pb07/diary">pb07</a>'
 	});
 
 // region IGNPhoto
 	var IGNPhoto = L.tileLayer('https://wxs.ign.fr/pratique/wmts/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg', {
 		maxZoom: 19,
-		attribution: '&copy; <a href="[[https://www.geoportail.gouv.fr/depot/api/cgu/CGU_API_libre.pdf|{{https://wxs.ign.fr/static/logos/IGN/IGN.gif|32}}]]">IGN Image aérienne</a> | <a href="https://www.openstreetmap.org/user/pb07/diary">pb07</a>'
+		attribution: '<a href="[[https://www.geoportail.gouv.fr/depot/api/cgu/CGU_API_libre.pdf|{{https://wxs.ign.fr/static/logos/IGN/IGN.gif|32}}]]">IGN Image aérienne</a> | Tracés: <a href="https://www.openstreetmap.org">OpenStreetMap</a> | <a href="https://www.openstreetmap.org/user/pb07/diary">pb07</a></a>'
 	});
 
 //endregion
