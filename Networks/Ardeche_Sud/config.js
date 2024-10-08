0338be30460b590f973f59233ab167b9f6d43041
pbalyx <40723752+pbalyx@users.noreply.github.com>
2024-10-08 17:08:13 +0200

    Update Networks\Ardeche_Sud\config

--- Networks/Ardeche_Sud/config.js
+++ Networks/Ardeche_Sud/config.js
@@ -1,5 +1,5 @@
 network_name = "Réseau Ardèche Sud";
-network_date = "2024-07-29";
+network_date = "2024-10-08";
 network_info = "Carte (encore incomplète) de sentiers balisés réunissant les réseaux de dix communautés de communes du Sud Ardèche";
 test_button = false;
 network_router = true;
